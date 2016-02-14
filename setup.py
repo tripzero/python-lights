c55035f772569a355891142325b14eb369c06a55
Kevron Rees <tripero.kev@gmail.com>
2016-02-14 14:39:35 -0800

    setup: use setuptools and specify requires

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, Extension
+
 setup(name='lights',
       version='1.0',
       description='Python Light module for working with color LEDs',
       author='Kevron Rees',
       author_email='tripzero.kev@gmail.com',
       url='https://github.com/tripzero/python-lights',
-      packages=["lights"]
+      packages=["lights"],
+      install_requires=["trollius"]
       )
