ac54449f0ef8cd2b53e2abb5c73db2210b9622bd
Kevron Rees <tripzero.kev@gmail.com>
2016-04-08 10:48:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@
 
 from setuptools import setup, Extension
 
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)
+Programming Language :: Python
+Topic :: System :: Hardware
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Unix
+Operating System :: POSIX :: Linux
+"""
+
 setup(name='lights',
       version='1.0',
       description='Python Light module for working with color LEDs',
@@ -9,5 +20,6 @@ setup(name='lights',
       author_email='tripzero.kev@gmail.com',
       url='https://github.com/tripzero/python-lights',
       packages=["lights"],
-      install_requires=["trollius"]
+      install_requires=["trollius"],
+      classifiers = filter(None, classifiers.split("\n"))
       )
