b30bdb47874c767a790b7d5ae3c43a6e310a2800
Kevron Rees <tripzero.kev@gmail.com>
2019-07-05 11:59:04 -0700

    add counter variable to test

--- test/bm_lightprotocol.py
+++ test/bm_lightprotocol.py
@@ -7,12 +7,13 @@ from fakelightarray import FakeLightArray2
 parser = photons.LightProtocol(leds=FakeLightArray2())
 
 message = parser.writeHeader(bytearray(binascii.unhexlify(b'010a000000ffff00010081ff00020000ff920300007bff04006a00ff05006100000600b500000700ff00000800ff00000900ff7700')))
+counter=1000000
 
 def do_test_parse():
 	parser.parse(message)
 
 def run_bm_parse():
-	t = timeit.timeit(stmt="do_test_parse()", setup="from __main__ import do_test_parse", number=1000000)
+	t = timeit.timeit(stmt="do_test_parse()", setup="from __main__ import do_test_parse", number=counter)
 
 	return t
 
@@ -20,3 +21,5 @@ if __name__ == "__main__":
 	
 	t = run_bm_parse()
 	print("do_test_update: {}".format(t))
+
+	print('frames per second: {}'.format(counter/t))
